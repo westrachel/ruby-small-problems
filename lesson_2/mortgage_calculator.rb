067a22f9034ab6a2d442a9e98aba891e290e8a13
R West <westrachel27@gmail.com>
2021-01-20 03:33:22 +0000

    Updated formatting conventions of mortgage calculator to appease rubocop

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -4,8 +4,9 @@ end
 
 def valid_to_two_decimals?(amt)
   if amt.include?(".") && amt != "" && amt.to_i > 0
-    amt.to_f.round(2).to_s == amt # ensure user doesn't input a dollar amount with nonsensical decimals 
-  else 
+    # ensure user doesn't input a dollar amount with nonsensical decimals
+    amt.to_f.round(2).to_s == amt
+  else
     amt.to_i.to_s == amt && amt != "" && amt.to_i > 0
   end
 end
@@ -13,14 +14,14 @@ end
 puts "Hello! Welcome to the Mortgage calculator."
 
 request("loan amount.")
-loan_amount = gets.chomp 
+loan_amount = gets.chomp
 
 until valid_to_two_decimals?(loan_amount)
   puts "That's not a valid loan amount. Enter your loan amount please."
   loan_amount = gets.chomp
 end
 
-request("Annual Percentage Rate (APR) as a whole number (ie 3.75 for 3.75%).") 
+request("Annual Percentage Rate (APR) as a whole number (ie 3.75 for 3.75%).")
 annual_apr = gets.chomp
 
 until valid_to_two_decimals?(annual_apr)
@@ -28,17 +29,20 @@ until valid_to_two_decimals?(annual_apr)
   annual_apr = gets.chomp
 end
 
-request("loan duration in years.") 
-duration_in_yrs = gets.chomp 
+request("loan duration in years.")
+duration_in_yrs = gets.chomp
 
 until valid_to_two_decimals?(duration_in_yrs)
-  puts "That doesn't seem right. Please enter the total number of years of your loan."
+  puts "That doesn't seem right.
+        Please enter the total number of years of your loan."
   duration_in_yrs = gets.chomp
 end
 
 duration_in_months = duration_in_yrs.to_i * 12 * -1
-monthly_interest_rate = annual_apr.to_f / 12 / 100 
-monthly_payment = (loan_amount.to_f * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(duration_in_months)))).round(2)
+monthly_interest_rate = annual_apr.to_f / 12 / 100
+monthly_payment = (loan_amount.to_f *
+                  (monthly_interest_rate / (1 -
+                  (1 + monthly_interest_rate)**(duration_in_months)))).round(2)
 
 mortgage_calc_result = <<-RESULT
   You're monthly payment is $#{monthly_payment} based on:
@@ -48,10 +52,3 @@ mortgage_calc_result = <<-RESULT
 RESULT
 
 puts mortgage_calc_result
-
-
-
-
-
-
-
