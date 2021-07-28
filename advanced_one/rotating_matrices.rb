95b1534777bc10848707e3777ce40c1f303bc1c4
EC2 Default User <ec2-user@ip-172-31-14-205.us-east-2.compute.internal>
2021-07-28 18:04:02 +0000

    finished rotating matrices problem

--- advanced_one/rotating_matrices.rb
+++ advanced_one/rotating_matrices.rb
@@ -15,38 +15,57 @@
 #  [6,  2,  8]
 #]
 
-# rotation:
+# rotation for 3x3 square matrix:
 # > first row (1st sub-array) becomes the 3rd column
 #      (= elements at the last index of each sub-array) in the rotated array
 # > second row (2nd sub-array) becomes the middle column
 # > third row (3rd sub-array) becomes the 1st column
 
+# rotation for non-square matrix:
+# [[3  4  1],
+#  [9  7  5]]
+
+# rotated matrix:
+# [[9, 3],
+#  [7, 4],
+#  [5, 1]]
+
+# notes: 
+#  > first column becomes first row
+#  > second column becomes second row
+#  > third column becomes third row
+
+# takeaways from the examples:
+# I want to essentially flip/transpose columns into rows
+# In prior problem, created a sub-method that can transpose, so can leverage that 
+
+
 # Algorithm:
-# 1. create a new empty array
-# 2. push empty sub-arrays to the empty array created in step 1
-#     <=> do this as many times as the number of elements in the passed in nested array
-# 3. initialize an index variable that points to the value zero
-# 4. iterate through each sub-array of the passed in nested array and w/in each iteration:
-#    > loop through the sub-array and push the sub-array's element to the relevant row
-#       aka empty sub-array of the newly created array (created in steps 1 and 2)
-#      <=> the first element of the sub-array should get pushed to the front of 1st sub-array
-#          of the newly created nested array
-# 5. return the newly rotated nested array
+# 1. Initialize an empty array to store new subarrays
+# 2. Want to select all elements at the same index of each subarray of the array argument
+#     > Invoke the times method on the size of the array argument's first subarray (to make sure that all elements are considered)
+#          <=> the times method's block's parameter will keep track of the index to select elements at from each subarray
+#     > within the block, initialize an empty subarray 
+#     > invoke the each method on the array argument
+#         >push each element at the index integer position of each subarray to the previously initialized subarray
+#     > on each iteration of the times method will have one sliced subarray that I want to push to the array that subarrays points to
+#          <=> push the reversed version of the sliced_subarray to the subarrays array
+# 3. Return the array that subarrays points to
 
 # Code:
+
 def rotate90(arr)
-  rotated = []
-  arr.size.times { rotated.push([]) }
-  idx = 0
-  arr.each do |sub_array|
-    until idx == sub_array.size
-      rotated[idx].insert(0, sub_array[idx])
-      idx += 1
+  subarrays = []
+  arr[0].size.times do |index|
+    sliced_subarray = []
+    arr.each do |sub_array|
+      sliced_subarray << sub_array[index]
     end
-    idx = 0
+    subarrays << sliced_subarray.reverse
   end
-  rotated
+  subarrays
 end
+
 
 # check work:
 matrix1 = [
@@ -65,5 +84,8 @@ new_matrix2 = rotate90(matrix2)
 new_matrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))))
 
 p new_matrix1 == [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
+# => true
 p new_matrix2 == [[5, 3], [1, 7], [0, 4], [8, 2]]
+# => true
 p new_matrix3 == matrix2
+# => true
